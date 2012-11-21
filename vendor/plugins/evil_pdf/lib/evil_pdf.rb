287503cac4c766f37d1786ac531b2706386a3fa3
Diego Salazar <diego@greyrobot.com>
2012-11-21 00:47:07 -0500

    pdf tweak

--- vendor/plugins/evil_pdf/lib/evil_pdf.rb
+++ vendor/plugins/evil_pdf/lib/evil_pdf.rb
@@ -12,13 +12,10 @@ class EvilPdf < WickedPdf
     t = Time.now.to_i
     root = "#{Rails.root}/tmp/pdf/"
     Dir.mkdir root unless Dir.exists? root
-    tmp_file = "#{root}#{t}.html"
     pdf_file = "#{root}#{options[:filename]}.pdf"
-    #File.open(tmp_file, 'w') { |f| f.puts s }
-    pdf = WickedPdf.new.pdf_from_string s, :pdf => pdf_file #`wkhtmltopdf #{tmp_file} #{pdf_file}`
+    pdf = WickedPdf.new.pdf_from_string s
     Rails.logger.debug "---> PDF done in #{Time.now.to_i - t} secs"
-    pdf = File.open(pdf_file, 'r').dup
-    `rm #{tmp_file} #{pdf_file}`
-    pdf
+    File.open(pdf_file, 'wb') { |f| f.puts pdf }
+    File.open(pdf_file, 'r')
   end
 end
