499497b965004e3b638d30c6de20baed7f9174d6
Diego Salazar <diego@greyrobot.com>
2012-11-26 22:02:20 -0500

    put wicked pdf in plugins

--- vendor/plugins/evil_pdf/lib/evil_pdf.rb
+++ vendor/plugins/evil_pdf/lib/evil_pdf.rb
@@ -1,14 +1,9 @@
 class EvilPdf < WickedPdf
   def self.generate(html, options = {})
-    (@evil_pdf ||= new).string_to_pdf html, options
+    (@evil_pdf ||= new).generate html, options
   end
   
-  def initialize
-    super
-    @exe_path = '/usr/local/Cellar/wkhtmltopdf/0.11.0_rc1/bin/wkhtmltopdf'
-  end
-  
-  def string_to_pdf(s, options = {})
+  def generate(s, options = {})
     t = Time.now.to_i
     root = "#{Rails.root}/tmp"
     Dir.mkdir root unless Dir.exists? root
@@ -16,10 +11,9 @@ class EvilPdf < WickedPdf
     Dir.mkdir pdf_dir unless Dir.exists? pdf_dir
     pdf_file = "#{pdf_dir}/#{options[:filename]}.pdf"
     
-    pdf = WickedPdf.new.pdf_from_string s
+    pdf = EvilPdf.new.pdf_from_string s
     
-    Rails.logger.fatal "---> PDF done in #{Time.now.to_i - t} secs"
+    Rails.logger.info "---> PDF done in #{Time.now.to_i - t} secs"
     File.open(pdf_file, 'wb') { |f| f.puts pdf }
-    File.open(pdf_file, 'r')
   end
 end
