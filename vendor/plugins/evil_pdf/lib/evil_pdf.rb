e4ccfac535fd6ec5c872714d8f50ff24245b723f
Diego Salazar <diego@greyrobot.com>
2012-11-28 23:36:34 -0500

    refactor

--- vendor/plugins/evil_pdf/lib/evil_pdf.rb
+++ vendor/plugins/evil_pdf/lib/evil_pdf.rb
@@ -13,10 +13,10 @@ class EvilPdf
   def from_urls(urls)
     @tmp_files = []
     urls.each_with_index do |url, i|
-      get_file(url) and generate(i)
+      retrieve(url) and generate(i)
     end
     combine
-    File.open file_name, 'r'
+    File.open file_path, 'r'
   end
   
   def from_urls!(urls)
@@ -28,14 +28,13 @@ class EvilPdf
     PDFKit.new(@html, @options).to_file name
   end
   
-  def file_name
-    @file_name ||= "./tmp/#{@record.name.parameterize}-#{Time.now.to_i}.pdf"
+  def file_path
+    @file_path ||= "./tmp/#{@record.name.parameterize}-#{Time.now.to_i}.pdf"
   end
   
   private
   
-  def get_file(url)
-    @filename = url.split('/').last
+  def retrieve(url)
     @html = open(url).read
   rescue OpenURI::HTTPError => e
     @record.errors.add :pdf, "HTTPError #{e.message}: #{url}"
@@ -51,7 +50,7 @@ class EvilPdf
   # using ghostscript to combine multiple pdfs into 1
   def combine
     gs_opts = "-q -dNOPAUSE -sDEVICE=pdfwrite"
-    gs_cmd = "gs #{gs_opts} -sOutputFile=#{file_name} -dBATCH #{@tmp_files.join(' ')}"
+    gs_cmd = "gs #{gs_opts} -sOutputFile=#{file_path} -dBATCH #{@tmp_files.join(' ')}"
     Rails.logger.debug "Combining PDFs: #{gs_cmd}"
     system gs_cmd
   end
